f8591bf9343d18596566563c0039360027e14c8f
therobgeorge <georg1rd@gmail.com>
2021-11-23 22:29:43 -0500

    adds guessing and stadiums to squad

--- src/views/Squad.vue
+++ src/views/Squad.vue
@@ -1,19 +1,37 @@
 <template>
   <div class="home">
     <h1>Random Squad</h1>
+    <h1
+      v-if="
+        correctGuesses.includes(stadium) &&
+        correctGuesses.includes(currentTeam.Conference) &&
+        correctGuesses.includes(currentTeam.Division) &&
+        ballers.length === correctBallers.length
+      "
+    >
+      YOU BALLED OUT
+    </h1>
     <img :src="currentTeam.WikipediaLogoUrl" alt="" />
     <h2>{{ currentTeam.City }} {{ currentTeam.Name }}</h2>
-    <p>guess params: {{ guessParams }}</p>
     <p>team object: {{ currentTeam }}</p>
+    <form v-on:submit.prevent="submit()">
+      <label>Guess</label>
+      <br />
+      <input type="string" v-model="guess" />
+      <input type="submit" value="Guess" />
+    </form>
+    <p>Stadium:</p>
+    <h1 v-if="correctGuesses.includes(stadium)">{{ stadium }}</h1>
     <p>Conference:</p>
-    <input type="string" v-model="guessParams.Conference" />
-    <h1 v-if="guessParams.Conference == currentTeam.Conference">YES</h1>
+    <h1 v-if="correctGuesses.includes(currentTeam.Conference)">{{ currentTeam.Conference }}</h1>
     <p>Division:</p>
-    <input type="string" v-model="guessParams.Division" />
-    <h1 v-if="guessParams.Division == currentTeam.Division">YES</h1>
+    <h1 v-if="correctGuesses.includes(currentTeam.Division)">{{ currentTeam.Division }}</h1>
     <p>Players</p>
     <div v-for="baller in ballers" v-bind:key="baller.id">
-      <p>{{ baller.YahooName }} {{ baller.Position }} {{ baller.guess }}</p>
+      <p v-if="correctBallers.includes(baller.LastName) || correctBallers.includes(baller.YahooName)">
+        {{ baller.YahooName }}
+      </p>
+      <p>{{ baller.Position }}</p>
     </div>
     <br />
     <button v-on:click="setCurrentTeam()">Set</button>
@@ -29,7 +47,10 @@ export default {
       teams: [],
       currentTeam: [],
       ballers: [],
-      guessParams: {},
+      guess: [],
+      correctGuesses: [],
+      correctBallers: [],
+      stadium: "",
     };
   },
   created: function () {
@@ -56,6 +77,31 @@ export default {
     setCurrentTeam: function () {
       this.currentTeam = this.teams[Math.floor(Math.random() * this.teams.length)];
       this.setCurrentBallers();
+      this.setCurrentStadium();
+    },
+    setCurrentStadium: function () {
+      axios
+        .get(`https://api.sportsdata.io/v3/nba/scores/json/Stadiums?key=c19b029585bf4933ac52af085391e1f5`)
+        .then((response) => {
+          console.log("stadiums", response.data);
+          this.stadium = response.data.find((stadium) => stadium.StadiumID === this.currentTeam.StadiumID).Name;
+        });
+    },
+    submit: function () {
+      if (
+        this.guess === this.currentTeam.Conference ||
+        this.guess === this.currentTeam.Division ||
+        this.guess === this.stadium
+      ) {
+        this.correctGuesses.push(this.guess);
+        this.guess = "";
+      } else if (
+        this.ballers.some((baller) => baller.LastName === this.guess) ||
+        this.ballers.some((baller) => baller.YahooName === this.guess)
+      ) {
+        this.correctBallers.push(this.guess);
+        this.guess = "";
+      }
     },
   },
 };
